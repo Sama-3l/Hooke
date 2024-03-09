eb59a20acec313f0c033e487a33d5c4a6e32279b
Ayush <me21b1076@iiitdm.ac.in>
2024-03-10 02:40:37 +0530

    Checkpoint

--- Api/readingImage.py
+++ Api/readingImage.py
@@ -1,18 +1,53 @@
 import easyocr
+from PIL import Image, ImageDraw
 import os
+import re
+
+def check_string_format(input_string):
+    pattern = re.compile(r'^(Q|q)[.\s]?\d+[\).]?$')
+    return bool(pattern.match(input_string))
+
+def findMinY(loc):
+    minY = 10000000000
+    for pos in loc:
+        if pos[1] < minY:
+            minY = pos[1]
+    return minY
 
 reader = easyocr.Reader(['en'])
 
-# Path to your image
-image_path = os.getcwd() + "\\Api\\Images\\page2.jpeg"
+dest_path = os.getcwd() + "\\public\\Question"
+os.makedirs(dest_path, exist_ok=True)
 
+Question_Number = 1
+image_dir_path = os.getcwd() + "\\Api\\Images\\"
 
-# Perform OCR on the image
-results = reader.readtext(image_path)
+# Loop for the images 
+for filename in os.listdir(image_dir_path):
+    image_path = os.path.join(image_dir_path, filename)
+    results = reader.readtext(image_path)
 
-print(results[0])
+    image = Image.open(image_path)
 
-# # Print the extracted text and its bounding box coordinates
-for result in results:
-    loc, *text = result
-    print(f"Text: {text}, Bounding Box: ({loc})")
+    y_cordinates = []
+
+    for result in results:
+        loc, text, _ = result
+        if check_string_format(text):
+            print(f"Text: {text}, {loc}")
+            y_cordinates.append(findMinY(loc))
+    # print(results[-1][0])
+    # print(findMinY(results[-1][0]))
+    # y_cordinates.append(findMinY(results[-1][0]))
+    y_cordinates.append(image.height)
+    print(y_cordinates)
+
+    for i in range(len(y_cordinates)-1):
+        y_start = y_cordinates[i] - 10
+        y_end = y_cordinates[i+1] - 10
+
+        crop_region = (0, y_start, image.width, y_end)
+        cropped_image = image.crop(crop_region)
+        cropped_image.save(os.path.join(dest_path, f"{Question_Number}.jpeg"))
+        Question_Number+=1
+    y_cordinates.clear()
