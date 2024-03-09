1fa995f8c50bcea5cd2b9051a62148c8611d6a6a
Ayush <me21b1076@iiitdm.ac.in>
2024-03-09 19:05:24 +0530

    Pdf pages Images

---
+++ Api/creatingImage.py
@@ -0,0 +1,16 @@
+from pdf2image import convert_from_path
+import os
+from PIL import Image
+
+pdf_path = os.getcwd() + "\\backend\\src\\testPaper.pdf"
+poppler_path = os.getcwd() + "\\poppler-24.02.0\\Library\\bin"  
+dest_path = os.getcwd() + "\\Api\\Images\\"
+
+
+print(poppler_path)
+try:
+    images = convert_from_path(pdf_path, poppler_path=poppler_path)
+    for i, image in enumerate(images):
+        image.save(dest_path +f'page{i}.jpeg', 'JPEG')
+except Exception as e:
+    print(f"Error: {e}")
